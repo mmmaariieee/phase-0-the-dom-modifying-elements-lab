99f759902d0dc35f2b7fb51d365503dd23576bd5
Mariia <m.goncharenkoo@icloud.com>
2022-09-26 01:20:35 -0400

    commit tree e4bb74b23347da5769e3459183954172a720fe8a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main =  document.querySelector('main');
+main.remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "MARIIA-HONCHARENKO is the champion";
